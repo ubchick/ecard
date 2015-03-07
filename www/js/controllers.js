7988558dcbfb5ac87b870038e4452e3380ceee68
Coco89 <courtney.m.christian@gmail.com>
2015-03-07 17:04:04 -0500

    got things half way working on the back end
    go the angular in and putting in json form, added action sheet, etc. lol

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,12 +1,46 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ionic'])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', ['$scope', '$state', function($scope, $state) {
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
+  var user = $scope.user;
+  $scope.master = {};
+  $scope.user = angular.copy($scope.master);
+
+  if ($rootScope.isLoggedIn) {
+        $scope.name = user.get('name');
+    }
+
+}])
+
+.controller('ChatsCtrl', function($scope, $ionicActionSheet, $timeout) {
+
+ // Triggered on a button click, or some other target
+ $scope.show = function() {
+
+   // Show the action sheet
+   var hideSheet = $ionicActionSheet.show({
+     buttons: [
+       { text: 'Call 911' },
+       { text: 'Send Location' },
+       { text: 'Text Help'}
+     ],
+     titleText: 'Send an Alert',
+     cancelText: 'Cancel',
+     cancel: function() {
+          // add cancel code..
+        },
+     buttonClicked: function(index) {
+       return true;
+     }
+   });
+
+   // For example's sake, hide the sheet after two seconds
+   $timeout(function() {
+     hideSheet();
+   }, 2000);
+
+ };
+
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
@@ -21,8 +55,17 @@ angular.module('starter.controllers', [])
   $scope.friend = Friends.get($stateParams.friendId);
 })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+.controller('AccountCtrl', ['$scope', '$state', function($scope, $state) {
+      $scope.master = {};
+
+      $scope.update = function(user) {
+        $scope.master = angular.copy(user);
+        $state.go('tab.dash');
+      };
+
+      $scope.reset = function() {
+        $scope.user = angular.copy($scope.master);
+      };
+
+      $scope.reset();
+}]);
